953768cf07fdd22cab8faac017e453155ad7a81f
Edmarques Lima <edmarquessantosdelima@gmail.com>
2023-02-08 18:16:57 -0300

    feat: componentize form add projetc

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,22 @@ const routes: RouteRecordRaw[] = [
     {
         path: "/",
         name: "Tarefas",
-        component: () => import("../views/Tarefas.vue"),
+        component: () => import("@/views/Tarefas.vue"),
     },
     {
         path: "/projetos",
         name: "Projetos",
-        component: () => import("../views/Projetos.vue"),
+        component: () => import("@/views/Projetos.vue"),
+    },
+    {
+        path: "/projetos/novo",
+        name: "Novo Projeto",
+        component: () => import("@/views/projetos/Formulario.vue"),
+    },
+    {
+        path: "/projetos/:id",
+        name: "Editar Projeto",
+        component: () => import("@/views/projetos/Formulario.vue"),
     },
 ];
 
